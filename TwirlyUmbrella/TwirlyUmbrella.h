105130bb958a0e48fabe3233da2c0ee08df95a90
kleveri2 <kleveri2@msu.edu>
2020-07-20 18:16:08 -0400

    Add project files.

---
+++ TwirlyUmbrella/TwirlyUmbrella.h
@@ -0,0 +1,35 @@
+
+// TwirlyUmbrella.h : main header file for the TwirlyUmbrella application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CTwirlyUmbrellaApp:
+// See TwirlyUmbrella.cpp for the implementation of this class
+//
+
+class CTwirlyUmbrellaApp : public CWinApp
+{
+public:
+	CTwirlyUmbrellaApp() noexcept;
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTwirlyUmbrellaApp theApp;
